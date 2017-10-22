945e163c66f27a30b0b04fd054729663389ab96e
James Gilles <jhgilles@mit.edu>
2017-10-22 15:25:31 -0400

    Update schema to handle multiple games.

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -7,18 +7,31 @@ export type EntityID = number;
 export type TeamID = number;
 
 /**
- * A turn id.
- * The first turn is turn 0, when entities and sectors are initially spawned.
+ * The ID of a game.
  */
-export type Turn = number;
+export type GameID = string;
 
+/**
+ * The key to a key-protected game.
+ */
+export type PlayerKey = string;
+
+/**
+ * A location in the world.
+ */
 export interface Location {
     x: number;
     y: number;
 }
 
+/**
+ * The type of an entity.
+ */
 export type EntityType = "thrower" | "statue" | "hedge";
 
+/**
+ * A map tile.
+ */
 export type MapTile = "G" | "D";
 
 /**
@@ -47,16 +60,30 @@ export interface SectorData {
 /**
  * The data for the map.
  * 
- * Coordinate system: 
+ * Note: .bcmap files are simply gzipped MapData JSON objects.
  */
 export interface MapData {
+    /**
+     * The name of the map.
+     */
+    name: string;
+
+    /**
+     * The height of the map in tiles.
+     */
     height: number;
+
+    /**
+     * The width of the map in tiles.
+     */
     width: number;
+
     /**
      * Map is partitioned into square Sectors
      * Each Sector stores Location of its top left corner and covers sector_size * sector_size area 
      */
     sector_size: number;
+
     /**
      * Slightly wacky indexing:
      * Indexed as [height - (y + 1)][x];
@@ -65,6 +92,16 @@ export interface MapData {
      * up is y, and right is x.
      */
     tiles: MapTile[][];
+
+    /**
+     * The initial entities on the map.
+     */
+    entities: EntityData[];
+
+    /**
+     * The initial sectors of the map.
+     */
+    sectors: SectorData[];
 }
 
 /**
@@ -72,7 +109,22 @@ export interface MapData {
  */
 export interface TeamData {
     id: TeamID;
-    name: string;
+
+    /**
+     * The name of the team.
+     */
+    teamName: string;
+
+    /**
+     * The name of the bot.
+     */
+    botName: string;
+
+    /**
+     * The key the player used to login to the game.
+     * unset if the player used no key.
+     */
+    key?: PlayerKey;
 }
 
 /**
@@ -80,66 +132,12 @@ export interface TeamData {
  */
 export const NEUTRAL_TEAM: TeamData = {
     id: 0,
-    name: 'neutral'
+    teamName: 'neutral',
+    botName: 'neutral'
 }
 
-/**
- * Request the server login a player.
- */
-export interface Login {
-    command: "login";
-    name: string;
-}
+// All possible actions.
 
-/**
- * Confirms that a player has been logged in.
- */
-export interface LoginConfirm {
-    command: "login_confirm";
-    name: string;
-    id: TeamID;
-}
-
-/**
- * Notifies clients that a game has started.
- */
-export interface GameStart {
-    command: "start";
-    map: MapData;
-    teams: TeamData[];
-}
-
-/**
- * Notifies clients about the current turn / world state.
- */
-export interface NextTurn {
-    command: "next_turn";
-    /**
-     * The first turn sent will be 0, and contain all entity data for the map.
-     */
-    turn: number;
-    changed: EntityData[];
-    dead: EntityID[];
-    changed_sectors: SectorData[];
-
-    successful: Action[];
-    failed: Action[];
-    reasons: string[];
-
-    next_team: TeamID;
-    winner?: TeamID;
-}
-/**
- * Tells server to perform a list of action.
- */
-export interface MakeTurn {
-    command: "make_turn";
-    /**
-     * Send the turn from the previous NextTurn received.
-     */
-    turn: number;
-    actions: Action[];
-}
 export interface MoveAction {
     action: "move";
     id: EntityID;
@@ -166,6 +164,189 @@ export interface DisintegrateAction {
 }
 
 /**
+ * The data for a completed match.
+ * Note: .bh17 files are gzipped MatchData JSON objects.
+ */
+export interface MatchData {
+    map: MapData;
+    winner: TeamData;
+    turns: NextTurn[];
+}
+
+///////////////////////
+// Client -> server. //
+///////////////////////
+
+/**
+ * Request to take part in a game.
+ */
+export interface Login {
+    command: "login";
+
+    /**
+     * The name of the bot's team.
+     * Note, the bot is allowed to lie; we'll fix it for it.
+     */
+    teamName: string;
+
+    /**
+     * The name the player desires to be called.
+     */
+    botName: string;
+    
+    /**
+     * The game the player desires to connect to.
+     * If this field is null, the server will make a new game and connect the client to it.
+     * If the environment variable BATTLECODE_GAME exists, clients should
+     * set this field to its value.
+     */
+    game?: GameID;
+
+    /**
+     * The key to the game, if the game is key-protected.
+     */
+    key?: PlayerKey;
+}
+
+/**
+ * Requests the server send all messages for all games.
+ * Used by the viewer.
+ */
+export interface SpectateAll {
+    command: "spectate_all";
+    game: GameID;
+}
+
+/**
+ * Tells server to perform a list of action.
+ */
+export interface MakeTurn {
+    command: "make_turn";
+    game: GameID;
+
+    /**
+     * Send the turn from the previous NextTurn received.
+     */
+    turn: number;
+    actions: Action[];
+}
+
+//////////////////////
+// Server -> client //
+//////////////////////
+
+/**
+ * Confirms that a player has been logged in.
+ */
+export interface LoginConfirm {
+    command: "login_confirm";
+    game: GameID;
+    name: string;
+    id: TeamID;
+}
+
+/**
+ * Notifies clients that a game has started.
+ */
+export interface GameStart {
+    command: "start";
+    game: GameID;
+    map: MapData;
+    teams: TeamData[];
+}
+
+/**
+ * Notifies clients about the current turn / world state.
+ */
+export interface NextTurn {
+    command: "next_turn";
+    game: GameID;
+    turn: number;
+
+    changed: EntityData[];
+    dead: EntityID[];
+    changed_sectors: SectorData[];
+
+    successful: Action[];
+    failed: Action[];
+    reasons: string[];
+
+    next_team: TeamID;
+    winner?: TeamID;
+}
+
+/**
+ * Notifies client that its message was not understood.
+ */
+export interface ClientError {
+    command: "error";
+    reason: string;
+}
+
+/**
+ * Sends the entire state of the world, for testing purposes.
+ */
+export interface Keyframe {
+    command: "keyframe";
+    map: MapData;
+    teams: TeamData[];
+}
+
+///////////////////
+// Extra control //
+///////////////////
+
+/**
+ * Create a game but do not participate.
+ * At the end of the game, a GameData will be sent, containing all the information for the game.
+ */
+export interface CreateGame {
+    command: "create_game";
+
+    /**
+     * The key to the server.
+     */
+    serverKey: string;
+
+    /**
+     * The teams that will be participating in the game.
+     * Note: player .botName will override the .botName set here.
+     * If keys are set, players will be assigned to teams based on keys.
+     */
+    teams: TeamData[];
+
+    /**
+     * The map the game will be played on.
+     * Use a string to load a built-in map file.
+     */
+    map: MapData[] | string;
+}
+
+export interface CreateGameConfirm {
+    command: "create_game_confirm";
+
+    game: GameID;
+}
+
+/**
+ * Data sent 
+ */
+export interface GameData {
+    command: "game_data";
+
+    /**
+     * The winner of the match.
+     */
+    winner: TeamData;
+
+    /**
+     * The completed match.
+     * A base64-encoded gzipped MatchData.
+     */
+    game_file: string;
+}
+
+/**
  * Action types
  */
 export type Action = MoveAction | PickupAction | ThrowAction | BuildAction | DisintegrateAction;
@@ -173,5 +354,5 @@ export type Action = MoveAction | PickupAction | ThrowAction | BuildAction | Dis
 /**
  * Message types
  */
-export type IncomingCommand = Login | MakeTurn;
-export type OutgoingCommand = LoginConfirm | GameStart | NextTurn;
+export type IncomingCommand = Login | MakeTurn | SpectateAll | CreateGame;
+export type OutgoingCommand = LoginConfirm | GameStart | NextTurn | ClientError | Keyframe;

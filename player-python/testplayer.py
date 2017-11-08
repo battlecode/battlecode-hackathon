a9a86c04c45b7fc7145a4b39212b73cf984c31a7
Joshua Segaran <jsegaran1@gmail.com>
2017-11-07 23:47:04 -0500

    Fixed more throwing errors on client and server

--- player-python/testplayer.py
+++ player-python/testplayer.py
@@ -8,6 +8,18 @@ rounds = 0
 
 start = time.clock()
 
+def nearest_glass_state(state, entity):
+    nearest_statue = None
+    nearest_dist = 10000
+    for other_entity in state.get_entities(entity_type=battlecode.STATUE):
+        dist = entity.location.distance_to(other_entity.location)
+        if(dist< nearest_dist):
+            dist = nearest_dist
+            nearest_statue = other_entity
+
+    return nearest_statue
+
+
 for state in game.turns():
     for entity in state.get_entities(team=state.my_team):
         if entity.team != state.my_team or not entity.can_act:
@@ -20,15 +32,17 @@ for state in game.turns():
             if entity.can_pickup(pickup_entity):
                 entity.queue_pickup(pickup_entity)
 
-        for direction in battlecode.Direction.all():
+        statue = nearest_glass_state(state, entity)
+        if(statue != None):
+            direction = entity.location.direction_to(statue.location)
             if entity.can_throw(direction):
                 entity.queue_throw(direction)
-            break
 
         for direction in battlecode.Direction.all():
             if entity.can_move(direction):
                 entity.queue_move(direction)
 
+    time.sleep(0.1)
 
 
 

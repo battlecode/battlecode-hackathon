a3b8be9896662043b4b38eb8439cc224d68d7ccb
Jamie Bloxham <jamie.a.bloxham@gmail.com>
2017-11-10 02:52:52 -0500

    Make jamie happy

---
+++ viewer/src/types.ts
@@ -0,0 +1,11 @@
+
+/* Enum for the status of a game, used by viewer GUI. */
+export type GameStatus = 'waiting' | 'running' | 'finished';
+
+/* Represents a currently loaded game in the GUI. */
+export interface ActiveGameInfo {
+    status: GameStatus;
+    mapName: string;
+    playerOne: string;
+    playerTwo: string;
+};
